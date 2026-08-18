51004f05287babdac6473bd658cef28949d727a1
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-08-18 21:41:38 +0100

    Jwt Autheticaion

--- src/main/java/security/JwtAuthenticationFilter.java
+++ src/main/java/security/JwtAuthenticationFilter.java
@@ -1,4 +1,22 @@
 package security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+    }
 }
